05971abe0ad11ae11ffe7cc8ea26af23838a0d4c
richard <richard.smith@megaslice.uk>
2022-11-04 21:25:28 +0000

    title

--- Timer.gd
+++ Timer.gd
@@ -8,6 +8,13 @@ func _on_Timer_timeout():
 		alien.position.x = rand_range(0, 410)
 		alien.position.y = rand_range(20, 120)
 		get_node("/root/Main/aliens").add_child(alien)
+		
+	get_node("/root/Main/Title").visible = true
+	for i in range(4):	
+		var s = get_node("/root/Main/Player"+str(i)).score
+		if s>0:
+			get_node("/root/Main/Title").visible = false
+		
 
 func _input(event):
 	if Input.is_action_pressed("ui_cancel"):
